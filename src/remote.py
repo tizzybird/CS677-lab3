b5168cba6d2a6463df337b9b50379b6f3ad60e72
tizzybird <yschen@livemail.tw>
2020-04-20 21:55:50 -0400

    temp implementation for heartbeat protocol. TODO: async requests

--- src/remote.py
+++ src/remote.py
@@ -1,49 +1,112 @@
 import json
 
-from multiprocessing import Value
+from multiprocessing import Value, Manager
+import multiprocessing as mp
+
+import asyncio
+# import requests as rq
+import time
+
+UNKNOWN = 0
+ONLINE  = 1
+HEARBEAT_GAP = 4
+
+reset_value = 999999
+
+########################################
+
+def init(servers):
+    # shared server status information
+    manager = Manager()
+    obj = manager.dict()
+    # normal static server information
+    result = []
+    count = 0
+    for server in servers:
+        result.append({
+            "addr": server["addr"],
+            "port": server["port"]
+        })
+        obj[count] = {
+            "status": UNKNOWN,
+            "retry": 0
+        }
+        count += 1
+
+    return result, obj
+
+
+def choose_server(servers, num):
+    index = 0
+    with num.get_lock():
+        index = num.value
+        num.value += 1
+        if num.value >= reset_value:
+            num.value -= reset_value
+
+    return index
+
+
+def get_server(server_type, logger=None):
+    servers = order_servers
+    num = order_num
+    status = order_status
+    if server_type == 'catalog':
+        servers = catalog_servers
+        num = catalog_num
+        status = catalog_status
+
+    while True:
+        index = choose_server(servers, num) % len(servers)
+        if status[index]["status"] == ONLINE:
+            break
+
+    server_ip = "http://%s:%d/" % (servers[index]["addr"], servers[index]["port"])
+
+    if logger is not None:
+        logger.debug(server_type, ' server is returned: %s' % server_ip)
+
+    return server_ip
+
+
+def heartbeat(pairs, logger):
+    # pairs = [(order_servers, order_status), (catalog_servers, catalog_status)]
+    while True:
+        for servers, status in pairs:
+            for index in range(len(servers)):
+                server = servers[index]
+
+                ip = "http://%s:%d/echo" % (server["addr"], server["port"])
+                try:
+                    res = rq.get(ip, timeout=5)
+                    status[index] = {
+                        "status": ONLINE,
+                        "retry": 0
+                    }
+                except:
+                    status[index] = {
+                        "status": UNKNOWN,
+                        "retry": status[index]["retry"] + 1
+                    }
+                
+                logger.info("%s - status: %s" % (ip, repr(status[index])))
+
+        time.sleep(4)
+
+
+def start_hearbeat(logger):
+    pairs = [(order_servers, order_status), (catalog_servers, catalog_status)]
+    logger.info("Starting heartbeat demon..")
+    p = mp.Process(target=heartbeat, args=(pairs, logger))
+    p.start()
+
+
+################################
+order_num = Value('i', 0)
+catalog_num = Value('i', 0)
 
 with open('config.json') as f:
     CONFIG = json.load(f)
 
-order_servers = CONFIG['ip']['order']
-catalog_servers = CONFIG['ip']['catalog']
-
-reset_value = 999999
-
-order_num = Value('i', 0)
-catalog_num = Value('i', 0)
-
-def get_order_ip(logger):
-    with order_num.get_lock():
-        index = order_num.value
-        order_num.value += 1
-    
-    if order_num.value >= reset_value:
-        with order_num.get_lock():
-            order_num.value -= reset_value
-
-    index %= len(order_servers)
-    res = "http://%s:%d/" % (CONFIG["ip"]["order"][index]["addr"],\
-        CONFIG["ip"]["order"][index]["port"])
-
-    logger.debug('order server %d is returned' % (index + 1))
-
-    return res
-
-
-def get_catalog_ip(logger):
-    with catalog_num.get_lock():
-        index = catalog_num.value
-        catalog_num.value += 1
-    
-    if catalog_num.value >= reset_value:
-        with catalog_num.get_lock():
-            catalog_num.value -= reset_value
-
-    index %= len(catalog_servers)
-    res = "http://%s:%d/" % (CONFIG["ip"]["catalog"][index]["addr"],\
-        CONFIG["ip"]["catalog"][index]["port"])
-
-    logger.debug('catalog server %d is returned' % (index + 1))
-
-    return res
+order_servers, order_status = init(CONFIG['ip']['order'])
+catalog_servers, catalog_status = init(CONFIG['ip']['catalog'])
