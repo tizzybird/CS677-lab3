36577715360ed9a69150198c2c876bdfd3e3a671
tizzybird <yschen@livemail.tw>
2020-04-27 20:22:58 -0400

    Modify termination script

--- src/terminate_elnux.sh
+++ src/terminate_elnux.sh
@@ -2,57 +2,57 @@
 . elnux_credentials.sh
 
 # terminate client
-ssh ${Username}@${ClientServer} TargetPath=$TargetPath 'bash -s' << 'END_SSH'
+ssh ${Username}@${ClientServer} TargetPath=$TargetPath Username=${Username} 'bash -s' << 'END_SSH'
 set -x
 cd $TargetPath
-cat client.pid > kill
 rm client.pid
+pkill -u $Username
 END_SSH
 
 # terminate frontend server
-ssh ${Username}@${FrontendServer} TargetPath=$TargetPath 'bash -s' << 'END_SSH'
+ssh ${Username}@${FrontendServer} TargetPath=$TargetPath Username=${Username} 'bash -s' << 'END_SSH'
 set -x
 cd ${TargetPath}
-cat frontend.pid > kill
 rm frontend.pid
+pkill -u $Username
 END_SSH
 
 # terminate catalog server 1
-ssh ${Username}@${CatalogServer1} TargetPath=$TargetPath 'bash -s' << 'END_SSH'
+ssh ${Username}@${CatalogServer1} TargetPath=$TargetPath Username=${Username} 'bash -s' << 'END_SSH'
 set -x
 cd ${TargetPath}
-cat catalog0.pid > kill
 rm catalog0.pid
+pkill -u $Username
 END_SSH
 
 # terminate catalog server 2
-ssh ${Username}@${CatalogServer2} TargetPath=$TargetPath 'bash -s' << 'END_SSH'
+ssh ${Username}@${CatalogServer2} TargetPath=$TargetPath Username=${Username} 'bash -s' << 'END_SSH'
 set -x
 cd ${TargetPath}
-cat catalog1.pid > kill
 rm catalog1.pid
+pkill -u $Username
 END_SSH
 
 # terminate order server 1
-ssh ${Username}@${OrderServer1} TargetPath=$TargetPath 'bash -s' << 'END_SSH'
+ssh ${Username}@${OrderServer1} TargetPath=$TargetPath Username=${Username} 'bash -s' << 'END_SSH'
 set -x
 cd ${TargetPath}
-cat order0.pid > kill
 rm order0.pid
+pkill -u $Username
 END_SSH
 
 # terminate order server 2
-ssh ${Username}@${OrderServer2} TargetPath=$TargetPath 'bash -s' << 'END_SSH'
+ssh ${Username}@${OrderServer2} TargetPath=$TargetPath Username=${Username} 'bash -s' << 'END_SSH'
 set -x
 cd ${TargetPath}
-cat order1.pid > kill
 rm order1.pid
+pkill -u $Username
 END_SSH
 
 # terminate lock server
-ssh ${Username}@${LockServer} TargetPath=$TargetPath 'bash -s' << 'END_SSH'
+ssh ${Username}@${LockServer} TargetPath=$TargetPath Username=${Username} 'bash -s' << 'END_SSH'
 set -x
 cd ${TargetPath}
-cat lock.pid > kill
 rm lock.pid
+pkill -u $Username
 END_SSH
